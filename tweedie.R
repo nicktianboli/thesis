cdcf5f7bd04503a34f4ef271a8df94d4fc336dcb
nicktianboli <li_tianbo@live.com>
2017-05-26 16:37:20 +0800

    update

--- tweedie.R
+++ tweedie.R
@@ -9,7 +9,7 @@ library(gridExtra)
 library(e1071)
 library(mxnet)
 
-data_dummy = read.csv('C:\\Users\\TIANBO001\\OneDrive\\master thesis\\data and R code\\data_dummy\'.csv')[,2:65]
+data_dummy = read.csv('C:\\Users\\TIANBO001\\OneDrive\\master thesis\\data and R code\\data_dummy\'.csv')[,2:68]
 data_dummy_by_year = data_dummy[,c(1:7,13:64)]
 data_dummy_by_year$year = as.matrix(data_dummy[,8:12])%*%matrix(1:5,5,1)+2010
 trees = 10
@@ -17,7 +17,7 @@ trees = 10
 ## main function
 boosting_para = function(train_data, test_data){ 
   small_train_x = train_data[,c(1:6,8:56)]
-  small_train_x[,c('O1','V4','V6')] = (small_train_x[,c('O1','V4','V6')] - 
+  small_train_x[,c('I4','I5','I6','O1','V4','V6')] = (small_train_x[,c('O1','V4','V6')] - 
                                                         matrix(colMeans(small_train_x[,c('O1','V4','V6')]), 
                                                                nrow = dim(small_train_x[,c('O1','V4','V6')])[1],
                                                                ncol = dim(small_train_x[,c('O1','V4','V6')])[2],
@@ -25,19 +25,19 @@ boosting_para = function(train_data, test_data){
                                                                                   nrow = dim(small_train_x[,c('O1','V4','V6')])[1],
                                                                                   ncol = dim(small_train_x[,c('O1','V4','V6')])[2],byrow = T)
   small_train_weight = train_data$I2
-  small_train_y = train_data$if_claim
+  small_train_y = train_data$inc
   
   small_test_weight = test_data$I2
   small_test_x = test_data[small_test_weight == 1,c(1:6,8:56)]
   small_test_x[,c('O1','V4','V6')] = (small_test_x[,c('O1','V4','V6')] - 
-                                        matrix(colMeans(small_test_x[,c('O1','V4','V6')]), 
-                                               nrow = dim(small_test_x[,c('O1','V4','V6')])[1],
-                                               ncol = dim(small_test_x[,c('O1','V4','V6')])[2],
-                                               byrow = T))/matrix(sqrt(diag(var(small_test_x[,c('O1','V4','V6')]))),
-                                                                  nrow = dim(small_test_x[,c('O1','V4','V6')])[1],
-                                                                  ncol = dim(small_test_x[,c('O1','V4','V6')])[2],byrow = T)
+                                                       matrix(colMeans(small_test_x[,c('O1','V4','V6')]), 
+                                                              nrow = dim(small_test_x[,c('O1','V4','V6')])[1],
+                                                              ncol = dim(small_test_x[,c('O1','V4','V6')])[2],
+                                                              byrow = T))/matrix(sqrt(diag(var(small_test_x[,c('O1','V4','V6')]))),
+                                                                                 nrow = dim(small_test_x[,c('O1','V4','V6')])[1],
+                                                                                 ncol = dim(small_test_x[,c('O1','V4','V6')])[2],byrow = T)
   
-  small_test_y =  test_data$if_claim[small_test_weight == 1]
+  small_test_y =  test_data$inc[small_test_weight == 1]
   
   trees = 10
   alpha = 0.5
